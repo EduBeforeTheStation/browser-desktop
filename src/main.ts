4c62ec4b641ba7576f142ce57e00c2c94f900ef7
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2021-07-17 09:25:15 +0900

    setting electron-builder

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ function createWindow() {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+  mainWindow.loadURL("https://duckduckgo.com/");
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();

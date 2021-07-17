7235bd28d2085912a67b844c15a59aacb1aacfaf
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2021-07-17 14:50:52 +0900

    create password script

---
+++ src/updater.ts
@@ -0,0 +1,62 @@
+import { app, BrowserWindow, dialog } from 'electron';
+import { autoUpdater as updater } from "electron-updater";
+
+export class Updater {
+    updateWin: BrowserWindow | undefined;
+    constructor() {
+        updater.on("checking-for-update", () =>
+            this.sendStatusToWindow("Checking for update..."));
+        updater.on("update-available", () => {
+            this.sendStatusToWindow("Update available.");
+            this.updateWin?.show();
+        });
+        updater.on("update-not-available", () => {
+            this.sendStatusToWindow("Update not available.");
+            this.updateWin?.close();
+        });
+        updater.on("error", (err) => {
+            this.sendStatusToWindow("Error in auto-updater. " + err);
+            this.updateWin?.show();
+        });
+        updater.on("download-progress", (progressObj) => {
+            let log_message = "Download speed: " + progressObj.bytesPerSecond;
+            log_message = log_message + " - Downloaded " + progressObj.percent + "%";
+            log_message = log_message + " (" + progressObj.transferred + "/" + progressObj.total + ")";
+            this.sendStatusToWindow(log_message);
+        });
+        updater.on("update-downloaded", () => {
+            this.sendStatusToWindow("Update downloaded");
+            if (!this.updateWin) return;
+
+            const option = {
+                type: "question",
+                buttons: ["업데이트", "취소"],
+                defaultId: 0,
+                title: "electron-updater",
+                message: "업데이트가 있습니다. 프로그램을 업데이트 하시겠습니까?",
+            };
+            const btnIndex = dialog.showMessageBoxSync(this.updateWin, option);
+
+            if (btnIndex === 0) {
+                updater.quitAndInstall();
+            }
+        });
+    }
+    createDefaultUpdateWindow(): BrowserWindow {
+        this.updateWin = new BrowserWindow({
+            backgroundColor: "#eeeeee",
+            webPreferences: { nodeIntegration: true, contextIsolation: false },
+            show: false
+        });
+
+        this.updateWin.on("closed", () => {
+            this.updateWin = undefined;
+        });
+        this.updateWin.loadURL(`file://${__dirname}/public/updater.html#v${app.getVersion()}`);
+        return this.updateWin;
+    }
+    sendStatusToWindow(text: string): void {
+        if (this.updateWin?.webContents)
+            this.updateWin?.webContents.send("message", text);
+    }
+}
--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,18 +1,25 @@
-import { } from 'node-machine-id';
+import { machineIdSync } from 'node-machine-id';
 import crpyto from 'crypto';
-import sha256 from 'crypto-js/sha256';
-import hmacSHA512 from 'crypto-js/hmac-sha512';
-import Base64 from 'crypto-js/enc-base64';
+import CryptoJS, { AES, SHA256 } from 'crypto-js';
 
 /*
 
 */
 
-export function encrypt(password: string): string {
-    
-    return '';
+const machineID = machineIdSync();
+
+export function shakeKey(password: string): string {
+    const key = password + machineID;
+    const hashkey = SHA256(key);
+    return hashkey.toString();
 }
 
-export function decrypt(password: string): string {
-    return '';
+export function encrypt(password: string, key: string): string {
+    const encrypted = AES.encrypt(password, key).toString();
+    return encrypted;
+}
+
+export function decrypt(encrypted: string, key: string): string {
+    const plain = AES.decrypt(encrypted, key).toString(CryptoJS.enc.Utf8);
+    return plain;
 }
